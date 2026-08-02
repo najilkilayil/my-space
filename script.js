62d0b0a9c280a838c0200674b8bc52f5adb4f4ee
Najil <najiljan26@gmail.com>
2026-08-02 12:49:49 +0530

    added Js function to get today/cuurent date

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ let sectionList = document.getElementById("sections_list")
 let msgInputEl = document.getElementById("chat_box")
 let subBtn = document.getElementById("chat_btn")
 
+let today = new Date()
+
+let day = today.getDate()
+let month = today.getMonth() + 1
+let year = today.getFullYear()
+
+let date = `${day}/${month}/${year}`
+
 subBtn.addEventListener("click", function() {
     let msg = msgInputEl.value
     sectionList.innerHTML += `
         <div class="section" id="section">
             <p id="msg">${msg}</p>
-            <p id="date">13/7/26</p>
+            <p id="date">${date}</p>
         </div>
     `
     msgInputEl.value = ''
