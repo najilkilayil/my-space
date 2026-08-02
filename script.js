6dd70e811c3515a57db5983e1130f70e695267fb
Najil <najiljan26@gmail.com>
2026-08-02 14:41:54 +0530

    Linked with LocalStorage

--- script.js
+++ script.js
@@ -3,21 +3,53 @@ let sectionList = document.getElementById("sections_list")
 let msgInputEl = document.getElementById("chat_box")
 let subBtn = document.getElementById("chat_btn")
 
-let today = new Date()
+// let today = new Date()
 
-let day = today.getDate()
-let month = today.getMonth() + 1
-let year = today.getFullYear()
+// let day = today.getDate()
+// let month = today.getMonth() + 1
+// let year = today.getFullYear()
 
-let date = `${day}/${month}/${year}`
+// let date = `${day}/${month}/${year}`
 
-subBtn.addEventListener("click", function() {
-    let msg = msgInputEl.value
-    sectionList.innerHTML += `
-        <div class="section" id="section">
-            <p id="msg">${msg}</p>
-            <p id="date">${date}</p>
-        </div>
-    `
+let messages = JSON.parse(localStorage.getItem("messages"))
+
+if (messages === null) {
+    messages = []
+}
+
+function renderMsg() {
+    sectionList.innerHTML = ""
+
+    messages.forEach(item => {
+        sectionList.innerHTML += `
+            <div class="section" id="section">
+                <p id="msg">${item.message}</p>
+                <p id="date">${item.date}</p>
+            </div>
+        `
+    });
+}
+
+subBtn.addEventListener("click", function () {
+
+    if (msgInputEl.value === "") {
+        return
+    }
+
+    let today = new Date()
+    let day = today.getDate()
+    let month = today.getMonth() + 1
+    let year = today.getFullYear()
+    let date = `${day}/${month}/${year}`
+ 
+    messages.push({
+        message: msgInputEl.value,
+        date: date
+    })
+
+    localStorage.setItem("messages", JSON.stringify(messages))
     msgInputEl.value = ''
+    renderMsg()
 })
+
+renderMsg()
